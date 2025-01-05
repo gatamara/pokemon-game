457c7b59b388143463621a9aeb13bcf6cccfc508
Tamara Salinas <dev.tamara29@gmail.com>
2025-01-05 18:36:30 -0300

    finish function

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -1,13 +1,22 @@
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { GameStatus, type Pokemon, type PokemonListResponse } from '../interfaces'
 import { pokemonApi } from '../api/pokemonApi'
+import confetti from 'canvas-confetti'
 
 export const usePokemonGame = () => {
   const gameStatus = ref<GameStatus>(GameStatus.Playing) //aqui vamos a tener 3 estados, donde mi estado inicial es Playing
+  const pokemons = ref<Pokemon[]>([])
+  const pokemonOptions = ref<Pokemon[]>([])
+
+  const randomPokemon = computed(() => {
+    const randomIndex = Math.floor(Math.random() * pokemonOptions.value.length)
+    return pokemonOptions.value[randomIndex]
+  })
+
+  const isLoading = computed(() => pokemons.value.length === 0)
 
   const getPokemons = async (): Promise<Pokemon[]> => {
     const response = await pokemonApi.get<PokemonListResponse>('/?limit=151')
-    console.log(response.data)
 
     const pokemonsArray = response.data.results.map((pokemon) => {
       const urlParts = pokemon.url.split('/')
@@ -17,16 +26,45 @@ export const usePokemonGame = () => {
         id: +id, //este + es para asegurarme que sea number
       }
     })
-    return pokemonsArray
+    return pokemonsArray.sort(() => Math.random() - 0.5) //los desordeno para el juego con sort que esta esperando un true o un false y Math.random
+  }
+
+  const getNextRound = (howMany: number = 4) => {
+    //tengo que tomar los primeros 4 elementos de mi arreglo, por que ya estan mezclados
+    gameStatus.value = GameStatus.Playing
+    pokemonOptions.value = pokemons.value.slice(0, howMany) //slice para cortar del arreglo de la pos 0 a howmany, almaceno los primeros 4, no modifica el arreglo
+    pokemons.value = pokemons.value.slice(howMany) //elimino los 4 y se almacenan todos los que quedan despues de eliminar los 4
+  }
+
+  const checkAnswer = (id: number) => {
+    const haswon = randomPokemon.value.id === id
+    if (haswon) {
+      gameStatus.value = GameStatus.Won
+      confetti({
+        particleCount: 600,
+        spread: 150,
+        origin: { y: 0.6 },
+      })
+      return
+    }
+    gameStatus.value = GameStatus.Lost
   }
 
   onMounted(async () => {
     // hay mas control con esto, aun que sepamos que se va a ejecutar cuando se monta, cuando pase el script setup
-    const pokemons = await getPokemons()
-    console.log(pokemons)
+
+    // await new Promise((r) => setTimeout(r, 1000))
+    pokemons.value = await getPokemons()
+    getNextRound()
   })
 
   return {
     gameStatus,
+    isLoading,
+    pokemonOptions,
+    randomPokemon,
+    //Methos
+    getNextRound,
+    checkAnswer,
   }
 }
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,14 +1,20 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Espere por favor</h1>
     <h3 class="animate-pulse">Cargando pokemons</h3>
   </section>
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
-    <h1>Quien es este Pokemon?</h1>
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-2xl m-5">Quien es este Pokemon?</h1>
+    <div class="h-20">
+      <button v-if="gameStatus !== GameStatus.Playing" class="btn" @click="getNextRound(4)">Jugar de nuevo?</button>
+    </div>
+
     <!-- pokemon picture -->
-    <PokemonPicture />
+    <PokemonPicture :pokemon-id="randomPokemon.id" :show-pokemon="gameStatus !== GameStatus.Playing" />
     <!-- pokemon options -->
-    <PokemonOptions />
+    <PokemonOptions :options="pokemonOptions" :block-selection="gameStatus !== GameStatus.Playing"
+      :correct-answer="randomPokemon.id" @selected-option="checkAnswer" />
   </section>
 </template>
 
@@ -16,9 +22,15 @@
 import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
 
-const { } = usePokemonGame()
+const { randomPokemon, isLoading, gameStatus, pokemonOptions, checkAnswer, getNextRound } = usePokemonGame()
+
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  @apply bg-cyan-500 rounded-md p-2 cursor-pointer text-center transition-all hover:bg-gray-100
+}
+</style>
